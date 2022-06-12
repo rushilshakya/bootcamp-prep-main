4f724632790c5f7a045588b4b0c93ce6abf864a7
rushilshakya <rushil.shakya@gmail.com>
2022-06-12 09:21:29 +0545

    feat: change to correct soln

--- lecture-notes/subash-wave.js
+++ lecture-notes/subash-wave.js
@@ -1,22 +1,25 @@
 function wave(str) {
   // Code here
-  let arr = [];
-  for (let i = 0; i < str.length; i++) {
-    arr.push(str.toLowerCase());
-  }
+  // let arr = [];
+  // for (let i = 0; i < str.length; i++) {
+  //   arr.push(str.toLowerCase());
+  // }
   // console.log(arr)
   let newString = [];
-  let currentString = "";
+  // let currentString = "";
   // console.log(arr.length)
-  for (let j = 0; j < arr.length; j++) {
-    currentString = arr[j];
-    for (k = 0; k < currentString.length; k++) {
-      newString.push(
-        currentString.replace(currentString[k], currentString[k].toUpperCase())
-      );
+  // for (let j = 0; j < arr.length; j++) {
+  // currentString = arr[j];
+  for (k = 0; k < str.length; k++) {
+    //You need to skip empty strings
+    if (str[k] !== " ") {
+      //This code will not work because each time only the first occurrence is replaced
+      // newString.push(str.replace(str[k], str[k].toUpperCase()));
+      newString.push(str.slice(0, k) + str[k].toUpperCase() + str.slice(k + 1));
     }
-    break;
   }
+  // break;
+  // }
   return newString;
 }
 console.log(wave("hello world"));
